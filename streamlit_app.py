2648fa11efb8b24383850927a3b41fee3066e48c
Rafa <rafafitrina@gmail.com>
2025-05-26 17:17:18 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,7 @@
 import streamlit as st
 
 st.title("NastarsBakery( ≧∀≦)ノ")
+st.write(hiiiii) 
 st.write("find your sweet food at this place ><")
 st.image("3de28c91a675305ab3f05292ef85682c.jpg")
     
